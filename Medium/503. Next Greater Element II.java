14c3db1cda1b1efac29a318df5d2da78ec21a8c6
Sakshi <sakshichoudhary148@gmail.com>
2025-08-17 18:55:53 +0530

    Next greater element 2

---
+++ Medium/503. Next Greater Element II.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n =nums.length;
+        int[] ans=new int[n];
+        Stack<Integer> s=new Stack<>();
+        for(int i=2*n-1;i>=0;i--)
+        {
+            while(!s.isEmpty() && s.peek()<=nums[i%n])
+            {
+                s.pop();
+            }
+            if(i<n)  //v imp
+            {
+                if(!s.isEmpty())
+                {
+                    ans[i]=s.peek();
+                }
+                else
+                {
+                    ans[i]=-1;
+                }
+            }
+            s.push(nums[i%n]);
+        }
+        return ans;
+    }
+}
